c27283b62742dc988b6f28121cbb911e16777044
subramanyam <srathesweet@gmail.com>
2020-09-07 17:45:40 +0530

    1.5

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  siteName: 'A blog starter for Gridsome',
-  siteDescription: "Bleda is a blog starter kit for Gridsome, the Vue.js static site generator. It's inspired by Attila for Ghost, and styled with Tailwind CSS.",
+  siteName: 'Natural Guardian',
+  siteDescription: "The site is about Indian Natural Independence fighters",
   siteUrl: 'https://gridsome-starter-bleda.netlify.com',
   titleTemplate: `%s | Bleda`,
   icon: 'src/favicon.png',
